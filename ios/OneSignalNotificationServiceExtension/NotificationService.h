01ea5f4434321435c0040a3d8e517f1a63f3cefc
SimBoi <dev@simboi.com>
2024-12-29 19:56:38 +0200

    onesignal sdk setup for ios

---
+++ ios/OneSignalNotificationServiceExtension/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  OneSignalNotificationServiceExtension
+//
+//  Created by Hamza Odeh on 29/12/2024.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
